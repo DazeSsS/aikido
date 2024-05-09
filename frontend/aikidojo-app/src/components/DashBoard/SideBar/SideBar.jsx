3a2b5628eaca00b910ad1dff7dbd679659244b6a
Vladislav <vladdik9@gmail.com>
2024-05-08 23:58:14 -1000

    created logout handler in sidebar; embedded google material icons (logout icon)

--- frontend/aikidojo-app/src/components/DashBoard/SideBar/SideBar.jsx
+++ frontend/aikidojo-app/src/components/DashBoard/SideBar/SideBar.jsx
@@ -2,11 +2,16 @@ import PropTypes from 'prop-types';
 
 import { useState } from 'react';
 
+import { deleteToken } from '../../../utils/authToken';
+
+import { useNavigate } from 'react-router-dom';
+
 import styles from './SideBar.module.css';
 
 const SideBar = ({ onTabClick }) => {
 
     const [selectedTab, setSelectedTab] = useState('groups');
+    const navigate = useNavigate();
 
     const handleTabClick = (tab) => {
         console.log(selectedTab);
@@ -14,37 +19,82 @@ const SideBar = ({ onTabClick }) => {
         onTabClick(tab);
     }
 
+    const handleLogout = () => {
+        deleteToken();
+        navigate('/login');
+    }
+
 
     return (
-        <>
-            <div className={styles['sidebar__container']}>
-                <div className={styles['sidebar-title__container']}>
-                    <h1>Дэшборд</h1>
-                </div>
+      <>
+        <div className={styles["sidebar__container"]}>
+          <div className={styles["sidebar-title__container"]}>
+            <h1>Дэшборд</h1>
+          </div>
 
-                <div className={styles['sidebar__navigation']}>
-                    <ul>
-                        <li className={selectedTab === 'groups' ? styles['navigation-item-active'] : styles['navigation-item']} onClick={() => handleTabClick('groups')}>Группы</li>
-                        <li className={selectedTab === 'schedule' ? styles['navigation-item-active'] : styles['navigation-item']} onClick={() => handleTabClick('schedule')}>Расписание тренировок</li>
-                        <li className={selectedTab === 'students' ? styles['navigation-item-active'] : styles['navigation-item']} onClick={() => handleTabClick('students')}>Список учеников</li>
-                        <li className={selectedTab === 'payments' ? styles['navigation-item-active'] : styles['navigation-item']} onClick={() => handleTabClick('payments')}>Чеки об оплате</li>
-                    </ul>
-                </div>
-                
-                <div className={styles['sidebar-profile__container']}>
-                <img className={styles['user-profile-avatar']} src="/user-sidebar-avatar.png" alt="" />
-                    <div className={styles['profile-name__container']}>
-                        <h2>Киссёмару Уэсиба</h2>
-                        <span>k.ueshiba@aikidojo.ru</span>
-                    </div>
+          <div className={styles["sidebar__navigation"]}>
+            <ul>
+              <li
+                className={
+                  selectedTab === "groups"
+                    ? styles["navigation-item-active"]
+                    : styles["navigation-item"]
+                }
+                onClick={() => handleTabClick("groups")}
+              >
+                Группы
+              </li>
+              <li
+                className={
+                  selectedTab === "schedule"
+                    ? styles["navigation-item-active"]
+                    : styles["navigation-item"]
+                }
+                onClick={() => handleTabClick("schedule")}
+              >
+                Расписание тренировок
+              </li>
+              <li
+                className={
+                  selectedTab === "students"
+                    ? styles["navigation-item-active"]
+                    : styles["navigation-item"]
+                }
+                onClick={() => handleTabClick("students")}
+              >
+                Список учеников
+              </li>
+              <li
+                className={
+                  selectedTab === "payments"
+                    ? styles["navigation-item-active"]
+                    : styles["navigation-item"]
+                }
+                onClick={() => handleTabClick("payments")}
+              >
+                Чеки об оплате
+              </li>
+            </ul>
+          </div>
 
-                    <div className={styles['logout-button']}>
-                            <img src="/logout.png" alt="" />
-                    </div>
-                </div>
+          <div className={styles["sidebar-profile__container"]}>
+            <img
+              className={styles["user-profile-avatar"]}
+              src="/user-sidebar-avatar.png"
+              alt=""
+            />
+            <div className={styles["profile-name__container"]}>
+              <h2>Киссёмару Уэсиба</h2>
+              <span>k.ueshiba@aikidojo.ru</span>
             </div>
-        </>
-    )
+
+            <div onClick={handleLogout} className={styles["logout-button"]}>
+              <span className="material-icons">logout</span>
+            </div>
+          </div>
+        </div>
+      </>
+    );
 };
 
 SideBar.propTypes = {

0d4399342aef46f06e33423e9cb283e2675ce965
Ivan <ivanrakov540@gmail.com>
2024-05-10 00:14:42 +0500

    add sending email with login data

--- backend/src/api/internal/serializers/user.py
+++ backend/src/api/internal/serializers/user.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.core.mail import send_mail
 from rest_framework import serializers
 from rest_framework.serializers import ModelSerializer
 
@@ -40,6 +42,32 @@ class StudentSerializer(UserSerializer):
         fields = UserSerializer.Meta.fields + ['parents', 'account']
 
     def create(self, validated_data):
-        validated_data['password'] = User.objects.make_random_password(8)
+        user_email = validated_data['email']
+        random_password = User.objects.make_random_password(8)
+        validated_data['password'] = random_password
+
         user = User.objects.create_user(**validated_data)
+
+        send_mail(
+            subject="Регистрация на AikiDojo",
+            message=(
+                'Поздравляем, ваш аккаунт был успешно зарегистрирован на AikiDojo!\n\n'
+                'Данные для входа:\n'
+                f'логин: {user_email}\n'
+                f'пароль: {random_password}\n\n'
+                'Если вы случайно получили это сообщение - не пугайтесь, мы просто группа студентов, '
+                'которая разрабатывает веб-сервис, и мы могли случайно указать ваш email при тестировании🙂'
+            ),
+            html_message=(
+                '<b>Поздравляем, ваш аккаунт был успешно зарегистрирован на AikiDojo!</b><br><br>'
+                'Данные для входа:<br>'
+                f'<i>логин:</i> {user_email}<br>'
+                f'<i>пароль:</i> {random_password}<br><br>'
+                'Если вы случайно получили это сообщение - не пугайтесь, мы просто группа студентов, '
+                'которая разрабатывает веб-сервис, и мы могли случайно указать ваш email при тестировании🙂'
+            ),
+            from_email=settings.EMAIL_HOST_USER,
+            recipient_list=[user_email, settings.EMAIL_HOST_USER]
+        )
+
         return user
--- backend/src/core/settings.py
+++ backend/src/core/settings.py
@@ -124,6 +124,12 @@ REST_FRAMEWORK = {
     ),
 }
 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = os.getenv('EMAIL_HOST')
+EMAIL_PORT = os.getenv('EMAIL_PORT')
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+EMAIL_USE_SSL = True
 
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/

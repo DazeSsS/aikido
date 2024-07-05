59c77e583b721829a7c1c2f99f1a016bae8d3317
Ivan <ivanrakov540@gmail.com>
2024-07-05 16:13:19 +0500

    add the ability to register user in django admin

--- backend/src/api/internal/admin/user.py
+++ backend/src/api/internal/admin/user.py
@@ -1,27 +1,107 @@
 from django.contrib import admin
-
+from django import forms
 from api.models import User
+
+
+class UserCreationForm(forms.ModelForm):
+    password1 = forms.CharField(label='Password', widget=forms.PasswordInput)
+    password2 = forms.CharField(label='Password confirmation', widget=forms.PasswordInput)
+
+    class Meta:
+        model = User
+        fields = (
+            'role',
+            'is_staff',
+            'gender',
+            'rang',
+            'email',
+            'phone_number',
+            'first_name',
+            'last_name',
+            'middle_name',
+            'date_of_birth',
+            'photo'
+        )
+
+    def clean_password2(self):
+        password1 = self.cleaned_data.get("password1")
+        password2 = self.cleaned_data.get("password2")
+        if password1 and password2 and password1 != password2:
+            raise forms.ValidationError("Passwords don't match")
+        return password2
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.set_password(self.cleaned_data["password1"])
+        if commit:
+            user.save()
+        return user
 
 
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
+    add_form = UserCreationForm
+    form = UserCreationForm
     readonly_fields = ('id',)
-    fields = (
-        'id',
-        'role',
-        'gender',
-        'rang',
-        'email',
-        'phone_number',
-        'first_name',
-        'last_name',
-        'middle_name',
-        'date_of_birth',
-        'photo'
+
+    list_display = ('email', 'first_name', 'last_name', 'is_staff')
+    search_fields = ('email', 'first_name', 'last_name')
+    ordering = ('email',)
+
+    fieldsets = (
+        (
+            None,
+            {
+                'fields': (
+                    'id',
+                    'role',
+                    'is_staff',
+                    'gender',
+                    'rang',
+                    'email',
+                    'phone_number',
+                    'first_name',
+                    'last_name',
+                    'middle_name',
+                    'date_of_birth',
+                    'photo',
+                    'password',
+                )
+            }
+        ),
     )
 
-    def get_fields(self, request, obj=None):
-        fields = list(self.fields)
-        if obj and obj.role != User.STUDENT:
-            fields.insert(2, 'is_staff')
-        return fields
+    add_fieldsets = (
+        (
+            None,
+            {
+                'classes': ('wide',),
+                'fields': (
+                    'id',
+                    'role',
+                    'is_staff',
+                    'gender',
+                    'rang',
+                    'email',
+                    'phone_number',
+                    'first_name',
+                    'last_name',
+                    'middle_name',
+                    'date_of_birth',
+                    'photo',
+                    'password1',
+                    'password2',
+                )
+            }
+        ),
+    )
+
+    def save_model(self, request, obj, form, change):
+        if 'password1' in form.cleaned_data:
+            obj.set_password(form.cleaned_data['password1'])
+        super().save_model(request, obj, form, change)
+
+    def get_fieldsets(self, request, obj=None):
+        if not obj:
+            return self.add_fieldsets
+        return super().get_fieldsets(request, obj)

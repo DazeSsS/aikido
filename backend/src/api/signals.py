c01d4ad08dc648c79ee8a8fcf3a5fcfbc4ef731f
Ivan <ivanrakov540@gmail.com>
2024-07-10 08:10:25 +0500

    small fixes

--- backend/src/api/signals.py
+++ backend/src/api/signals.py
@@ -68,14 +68,16 @@ def sync_students_with_calendar(sender, instance, action, pk_set, **kwargs):
     if action == 'post_add':
         future_practices = Practice.objects.filter(date__gte=datetime.now())
         for practice in future_practices:
-            event_id = practice.event.id
-            cal.add_event_attendees(event_id=event_id, new_attendees=gmails)
+            if hasattr(practice, 'event'):
+                event_id = practice.event.id
+                cal.add_event_attendees(event_id=event_id, new_attendees=gmails)
     
     if action == 'post_remove':
         future_practices = Practice.objects.filter(date__gte=datetime.now())
         for practice in future_practices:
-            event_id = practice.event.id
-            cal.remove_event_attendees(event_id=event_id, attendees_to_remove=gmails)
+            if hasattr(practice, 'event'):
+                event_id = practice.event.id
+                cal.remove_event_attendees(event_id=event_id, attendees_to_remove=gmails)
 
 
 @receiver(pre_delete, sender=Practice)
